0ff4cee572ca84c8a082f4f9205a97e841845b62
Bruno Gomes <brunog.vix@gmail.com>
2021-09-28 23:24:46 -0300

    header done

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,11 +1,27 @@
 import React from 'react';
-import {View, Text} from 'react-native';
+import { View, Text } from 'react-native';
 
-function Home(){
-    return(
-        <View>
-            <Text>Home</Text>
-        </View>
+import Header from '../../components/Header';
+import { Container, SearchContainer, SearchButton, Input } from './styles';
+import { Feather } from '@expo/vector-icons'
+
+
+function Home() {
+    return (
+        <Container>
+            <Header title="Movie Indicator" />
+
+            <SearchContainer>
+
+                <Input placeholder="Ex Vindagores" />
+
+
+                <SearchButton>
+                    <Feather name="search" size={30} color="#fff" />
+                </SearchButton>
+            </SearchContainer>
+
+        </Container>
     )
 }
 
